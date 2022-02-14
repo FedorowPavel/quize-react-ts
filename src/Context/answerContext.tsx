42c8b9ff5302bf2655e62f8f13ec4177d3293aca
FedorowPavel <pavel.fedorov@exposit.com>
2022-02-14 15:06:02 +0300

    removed unused files

--- src/Context/answerContext.tsx
+++
@@ -1,5 +0,0 @@
-import {createContext} from "react";
-
-export const IsAnsweredContext = createContext<[boolean, any] | undefined>(undefined)
-
-export const CorrectnessContext = createContext<[boolean, any] | undefined>(undefined)
--- src/Context/countriesContext.tsx
+++
@@ -1,4 +0,0 @@
-import {createContext} from "react";
-import {Country} from "../models/country";
-
-export const CountriesContext = createContext<Country[] | undefined>(undefined)
