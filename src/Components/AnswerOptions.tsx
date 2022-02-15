c1071802ca3901bd33effc1b0d48430b60f3e88d
FedorowPavel <pavel.fedorov@exposit.com>
2022-02-15 11:06:13 +0300

    answer options styles logic implementation changed using makeStyles

--- src/Components/AnswerOptions.tsx
+++ src/Components/AnswerOptions.tsx
@@ -5,31 +5,38 @@ import {getPrefix} from "../utils";
 import {QuizActionsEnum} from "../models/quiz";
 import {CorrectnessIndicatorIcon} from "./CorrectnessIndicatorIcon";
 import {useQuiz} from "../CustomHooks/useQuiz";
+import {makeStyles} from "@mui/styles";
 
-const cardStyles = {
-  padding: '14px',
-  marginBottom: '24px',
-  borderRadius: '12px',
-  border: '2px solid rgba(96, 102, 208, 0.7)',
-  boxShadow: 'none',
-  color: 'rgba(96, 102, 208, 0.8)',
-  fontSize: '18px',
-  display: 'flex',
-  columnGap: '40px',
-  "&:hover": {
-    backgroundColor: 'rgba(249, 168, 38, 1)',
-    color: 'white',
-    border: '2px solid rgba(249, 168, 38, 1)',
-    cursor: 'pointer'
+const useStyles = makeStyles({
+  card: {
+    padding: '14px',
+    marginBottom: '24px',
+    borderRadius: '12px',
+    border: '2px solid rgba(96, 102, 208, 0.7)',
+    boxShadow: 'none',
+    color: 'rgba(96, 102, 208, 0.8)',
+    fontSize: '18px',
+    display: 'flex',
+    columnGap: '40px',
+    "&:hover": {
+      backgroundColor: 'rgba(249, 168, 38, 1)',
+      color: 'white',
+      border: '2px solid rgba(249, 168, 38, 1)',
+      cursor: 'pointer'
+    },
+    "& span:nth-of-type(2)": {
+      flexGrow: '1'
+    }
   },
-  "span:nth-of-type(2)": {
-    flexGrow: '1'
-  }
-}
+  cardCorrect: {backgroundColor: '#60BF88', color: 'white', border: '2px solid #60BF88'},
+  cardNotCorrect: {backgroundColor: '#EA8282', color: 'white', border: '2px solid #EA8282'}
+})
 
 const AnswerOptions: React.FC<{options: IOption[]}> = ({options}): JSX.Element => {
   const {state, dispatch} = useQuiz()
   const [targetAnswer, setTargetAnswer] = useState<string>()
+
+  const classes = useStyles();
 
   const answerHandler = (option: IOption) => {
     if(state.isAnswered) {
@@ -42,12 +49,12 @@ const AnswerOptions: React.FC<{options: IOption[]}> = ({options}): JSX.Element =
     }
   }
 
-  const getStyles = useCallback((option: IOption) => {
+  const getClasses = useCallback((option: IOption) => {
     if(option.isCorrect && state.isAnswered) {
-      return {backgroundColor: '#60BF88', color: 'white', border: '2px solid #60BF88'}
+      return classes.cardCorrect
     }
     if(!option.isCorrect && state.isAnswered && targetAnswer === option.value) {
-      return {backgroundColor: '#EA8282', color: 'white', border: '2px solid #EA8282'}
+      return classes.cardNotCorrect
     }
   }, [options, state.isAnswered])
 
@@ -58,8 +65,7 @@ const AnswerOptions: React.FC<{options: IOption[]}> = ({options}): JSX.Element =
           return (
             <Card
               key={index}
-              sx={cardStyles}
-              style={getStyles(option)}
+              className={[classes.card, getClasses(option)].join(' ')}
               onClick={() => answerHandler(option)}
             >
               <span>{getPrefix(index)}</span>
